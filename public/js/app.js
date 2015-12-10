36b994a53e47ff3af1277799932091c6661eaeb7
atiannicelli <atiannicelli@gmail.com>
2015-12-10 15:05:02 -0500

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,9 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room') || '';
 var socket = io();
+
+console.log(name + ' wants to join ' + room);
+
 
 socket.on('connect', function () {
 	console.log('connected to socket.io server');
@@ -8,7 +13,9 @@ socket.on('message', function (message) {
 	var momentTimestamp = moment.utc(message.timestamp);
 	console.log('New message: ' + message.text);
 
-	jQuery('.messages').append('<p><strong>'+momentTimestamp.local().format('h:mma')+': </strong>' + message.text + '</p>');
+	var $message = jQuery('.messages');
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mma') + ': </strong></p>')
+	$message.append('<p>' + message.text + '</p>');
 });
 
 // Handles Submitting of new message
@@ -18,6 +25,7 @@ $form.on('submit', function(event) {
 	event.preventDefault();
 	var $message = $form.find('input[name=message]')
 	socket.emit('message', {
+		name: name,
 		text: $message.val()
 	});
 	$message.val('');
