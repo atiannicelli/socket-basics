56f358da1ff032af8a2e628ed20f50dd715b91c5
atiannicelli <atiannicelli@gmail.com>
2015-12-10 10:58:52 -0500

    add message to browser UI

--- public/js/app.js
+++ public/js/app.js
@@ -6,4 +6,18 @@ socket.on('connect', function () {
 
 socket.on('message', function (message) {
 	console.log('New message: ' + message.text);
+
+	jQuery('.messages').append('<p>' + message.text + '</p>');
+});
+
+// Handles Submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+	event.preventDefault();
+	var $message = $form.find('input[name=message]')
+	socket.emit('message', {
+		text: $message.val()
+	});
+	$message.val('');
 });
